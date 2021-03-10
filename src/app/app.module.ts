f221fa0e7a1fb61afb16d53ac38af715594361e3
Claudio Ferreira <claudio.zero0@gmail.com>
2021-03-10 19:48:38 -0300

    feat(header/footer) criando component de header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavHeaderComponent } from './components/nav-header/nav-header.component';
+import { NavFooterComponent } from './components/nav-footer/nav-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent,
+    NavFooterComponent
   ],
   imports: [
     BrowserModule,
